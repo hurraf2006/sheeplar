5e70edbe6f96a5928a5809a57ea59be7d40e717b
faruq <hurraf2006@gmail.com>
2021-11-18 15:10:01 +0500

    qoylar

---
+++ script.js
@@ -0,0 +1,28 @@
+let qsoni = +prompt("Qo'ylar sonini kiriting:")
+while(isNaN(qsoni) || qsoni===0) {
+    qsoni = +prompt("Qo'ygayam o'rgatsa sonni harfdan ajrata oladi🗿🗿🗿")
+}
+let t = ""
+
+for (let i = 1; i <=qsoni; i++) {
+    
+       t+=i
+ 
+        if (1==t) {
+            t+=(" sheepcha "); 
+        }
+        else if (2<=qsoni) {
+            t+=(" sheepchalar ");
+        }
+    
+         
+  
+
+
+   
+   
+     }
+     console.log(t);
+
+    
+
